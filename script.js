27e50d96d04f91dd2b6bc61c4c6562e3a9f6e338
diyor011 <104572938+diyor011@users.noreply.github.com>
2025-06-21 11:08:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let mevalar = [ 'Olma', 'Banan', 'Gilos', 'Shaftoli']
+console.log(mevalar);
+
+alert('Boshlanishda arrayning uzunligi: 4')
+alert("Sizning arrayingizdan malumot olib tashlamoqchimiz")
+
+alert('Arrayingizning uzunliki: 3')
+
+let mevalar2 = ['Olma', 'Banan', 'Gilos']
+// mevalar2.push('Apilsin')
+// mevalar2.push('Avakado')
+
+
+console.log(mevalar2);
+
+
